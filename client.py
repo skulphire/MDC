620347e8976d09217cc665dd21bff012cc456de3
Creghton <cdkrobotics@gmail.com>
2018-04-03 16:54:40 -0400

    Fixed server-client communication, now determine how to parse

--- client.py
+++ client.py
@@ -3,3 +3,8 @@ from scripts import SocketHandler
 if __name__ == '__main__':
     socket = SocketHandler.SocketHandle()
     socket.clientConnect()
+    socket.clientSendRecv("Messsage 1")
+    socket.clientSendRecv("Messsage 0")
+    socket.clientSendRecv("Messsage 2")
+    socket.clientSendRecv("Messsage 3")
+    socket.clientClose()
--- scripts/SocketHandler.py
+++ scripts/SocketHandler.py
@@ -2,28 +2,36 @@ from socket import *
 
 class SocketHandle(object):
     def __init__(self):
+        # server: "107.180.44.223"
+        # 184 is desktop
         self.s = socket(AF_INET,  SOCK_STREAM)
+
     def clientConnect(self):
         try:
-            #server: "107.180.44.223"
-            self.s.connect(("192.168.1.184", 8889))
+            self.s.connect(("192.168.1.69", 8889))
             print("connected")
-            self.s.send(self.convertToBytes("testing"))
-            data = self.convertToString(self.s.recv(1024))
-            print("server: "+data)
-            if data == "Recieved":
-                self.s.send(self.convertToBytes("done"))
-                print("Done")
-                self.s.close()
-            else:
-                print("No good")
-                self.s.close()
         except Exception:
             print("Could not connect")
 
+    def clientSendRecv(self, message):
+        self.s.send(self.convertToBytes(message))
+        data = self.convertToString(self.s.recv(1024))
+        print("server: " + data)
+
+    def clientClose(self):
+        self.s.send(self.convertToBytes("done"))
+        data = self.convertToString(self.s.recv(1024))
+        print("server: " + data)
+        if data == "Recieved":
+            print("Done")
+            self.s.close()
+        else:
+            print("<")
+            #self.s.close()
+
     def createServer(self):
         server = socket(AF_INET, SOCK_STREAM)
-        server.bind(("192.168.1.184",8889))
+        server.bind(("192.168.1.69",8889))
 
 
         while True:
@@ -37,10 +45,14 @@ class SocketHandle(object):
             while connection:
                 try:
                     data = connection.recv(1024)
-                    print("Client: "+self.convertToString(data))
-                    connection.sendall(self.convertToBytes("Recieved"))
+                    if data:
+                        print("Client: "+self.convertToString(data))
+                    if data == "done":
+                        connection.sendall(self.convertToBytes("Recieved"))
+                    else:
+                        connection.sendall(self.convertToBytes(">"))
                 except Exception:
-                    print("Finished")
+                    #print("Finished")
                     connection.close()
                     break
 
