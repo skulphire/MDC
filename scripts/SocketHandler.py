70bab5f3387231d670510d87481419ced2dae948
aphire <cdkrobotics@gmail.com>
2018-04-03 21:50:25 -0400

    testing server on internet addresses

--- scripts/SocketHandler.py
+++ scripts/SocketHandler.py
@@ -9,7 +9,7 @@ class SocketHandle(object):
 
     def clientConnect(self):
         try:
-            self.s.connect(("35.174.156.19", 9130))
+            self.s.connect(("ec2-35-174-156-19.compute-1.amazonaws.com", 9130))
             print("connected")
             return True
         except Exception:
@@ -34,7 +34,7 @@ class SocketHandle(object):
 
     def createServer(self):
         server = socket(AF_INET, SOCK_STREAM)
-        server.bind(("172.31.82.100",9130))
+        server.bind(("",9130))
         print("port:",server.getsockname())
 
         while True:
