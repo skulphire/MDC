f04d9d5aff3002d5a68275594d3e04f9a496b787
aphire <cdkrobotics@gmail.com>
2018-04-06 20:59:45 -0400

    testing logging in

--- scripts/serverSelect.py
+++ scripts/serverSelect.py
@@ -68,7 +68,7 @@ class serverHandle(object):
                             self.clients[s.getpeername()] = username[0]
                             print("Logged in")
                             print("   %s: %s" % (self.clients[peer], self.convertToString(data)))
-                            s.send("Valid")
+                            s.send(self.convertToBytes("Valid"))
                             if s not in self.outputs:
                                 self.outputs.append(s)
                         #if already logged on, handle data
