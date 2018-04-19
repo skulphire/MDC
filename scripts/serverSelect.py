72bb8dbcf33b3f1d224184d6be1f42b95a31d2cc
aphire <cdkrobotics@gmail.com>
2018-04-19 18:12:07 -0400

    testing logging in

--- scripts/serverSelect.py
+++ scripts/serverSelect.py
@@ -121,7 +121,7 @@ class serverHandle(object):
                                         badge = user.split(".")
                                         sending = sending+":"+badge[0]
                                 s.send(self.convertToBytes(sending))
-                            if "loggedinusers" not in message.lower():
+                            if "loggedinusers" and "blip" not in message.lower():
                                 print("   %s: %s" % (self.clientText[s], message))
                             if s not in self.outputs:
                                 self.outputs.append(s)
